b5e1bb250d010abb0bc47bb2e74df214886e835b
Fabio <fabiocruzcoelho@gmail.com>
2026-05-06 11:35:41 -0300

    chore: added wezterm, change config starship
    Signed-off-by: Fabio <fabiocruzcoelho@gmail.com>

---
+++ wezterm/config.lua
@@ -0,0 +1,94 @@
+local wezterm = require("wezterm")
+local config = {}
+
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+config = {
+  default_cursor_style = "BlinkingUnderline",
+  automatically_reload_config = true,
+  window_close_confirmation = "NeverPrompt",
+  adjust_window_size_when_changing_font_size = false,
+  cursor_blink_ease_in = "Constant",
+  cursor_blink_ease_out = "Constant",
+  window_decorations = "NONE",
+  check_for_updates = false,
+  use_fancy_tab_bar = false,
+  tab_bar_at_bottom = true,
+  cursor_blink_rate = 400,
+  font_size = 11.5,
+  font = wezterm.font("JetBrains Mono"),
+  enable_tab_bar = false,
+  window_padding = {
+    left = 3,
+    right = 3,
+    top = 0,
+    bottom = 0,
+  },
+  background = {
+    {
+      source = {
+        File = "/Users/hendrik/code/dotfiles/img/bg-monterey.png",
+      },
+      hsb = {
+        hue = 1.0,
+        saturation = 1.02,
+        brightness = 0.25,
+      },
+      width = "100%",
+      height = "100%",
+    },
+    {
+      source = {
+        Color = "#000000",
+      },
+      width = "100%",
+      height = "100%",
+      opacity = 0.95,
+    },
+  },
+  -- from: https://akos.ma/blog/adopting-wezterm/
+  hyperlink_rules = {
+    -- Matches: a URL in parens: (URL)
+    {
+      regex = "\\((\\w+://\\S+)\\)",
+      format = "$1",
+      highlight = 1,
+    },
+    -- Matches: a URL in brackets: [URL]
+    {
+      regex = "\\[(\\w+://\\S+)\\]",
+      format = "$1",
+      highlight = 1,
+    },
+    -- Matches: a URL in curly braces: {URL}
+    {
+      regex = "\\{(\\w+://\\S+)\\}",
+      format = "$1",
+      highlight = 1,
+    },
+    -- Matches: a URL in angle brackets: <URL>
+    {
+      regex = "<(\\w+://\\S+)>",
+      format = "$1",
+      highlight = 1,
+    },
+    -- Then handle URLs not wrapped in brackets
+    {
+      -- Before
+      --regex = '\\b\\w+://\\S+[)/a-zA-Z0-9-]+',
+      --format = '$0',
+      -- After
+      regex = "[^(]\\b(\\w+://\\S+[)/a-zA-Z0-9-]+)",
+      format = "$1",
+      highlight = 1,
+    },
+    -- implicit mailto link
+    {
+      regex = "\\b\\w+@[\\w-]+(\\.[\\w-]+)+\\b",
+      format = "mailto:$0",
+    },
+  },
+}
+return config
