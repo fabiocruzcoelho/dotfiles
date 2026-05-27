b0e5044439695864aa29841b81f9d5a15e9c4956
Fabio <fabiocruzcoelho@gmail.com>
2026-05-27 18:45:52 -0300

    chore: added exports AWS envs, remove env KUBECONFIG from mise
    Signed-off-by: Fabio <fabiocruzcoelho@gmail.com>

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,4 +1,7 @@
-
-export AWS_PROFILE=default
+export AWS_PROFILE=estudosdevops
+export AWS_DEFAULT_REGION=us-east-1
 export PATH="$HOME/dotfiles/bin:$PATH"
 
+# Proxmox VE API credentials
+export PROXMOX_VE_ENDPOINT="https://proxmox.homelab.tech"
+export PROXMOX_VE_API_TOKEN="terraform@pve!iac=eeb398b2-6eb9-470a-98dd-394ae1095937"
